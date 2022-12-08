185ba6a81558d91e60c95978b3e41d3f45e536d7
Faharet <73357375+Faharet@users.noreply.github.com>
2022-12-08 10:54:22 +0600

    Farkhad
    Killing people fixed

--- Assets/Scripts/Main Character/ObstaclePlayer.cs
+++ Assets/Scripts/Main Character/ObstaclePlayer.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 public class ObstaclePlayer : MonoBehaviour
 {
     private void OnTriggerStay2D(Collider2D col) {
-        if(col != null) {
-            if(col.gameObject == Enemy.Instance.gameObject && FindObjectOfType<MainCharacter>()._isAttacking) {
-                Enemy.Instance.GetDamage();
-            }
-        }
+       if(col.gameObject.tag == "Enemy") {
+           col.gameObject.GetComponent<Enemy>().GetDamage();
+       } 
     }
 }
