172c0c64dcf1624c5d06f3eb87d68e75c916b434
Faharet <73357375+Faharet@users.noreply.github.com>
2022-12-08 11:34:12 +0600

    Farkhad
    Little fix

--- Assets/Scripts/Main Character/ObstaclePlayer.cs
+++ Assets/Scripts/Main Character/ObstaclePlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ObstaclePlayer : MonoBehaviour
 {
     private void OnTriggerStay2D(Collider2D col) {
-       if(col.gameObject.tag == "Enemy") {
+       if(col.gameObject.tag == "Enemy" && FindObjectOfType<MainCharacter>()._isAttacking) {
            col.gameObject.GetComponent<Enemy>().GetDamage();
        } 
     }
