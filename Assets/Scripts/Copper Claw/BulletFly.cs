33077c6b966d2a31660e5ce54e4e9bdfa6346021
Faharet <73357375+Faharet@users.noreply.github.com>
2022-12-09 07:00:55 +0600

    Farkhad
    Bullet Attack and Bullet Trigger Fix

--- Assets/Scripts/Copper Claw/BulletFly.cs
+++ Assets/Scripts/Copper Claw/BulletFly.cs
@@ -7,6 +7,17 @@ public class BulletFly : MonoBehaviour
     [SerializeField] private float _speed;
     private float _lifeTime = 4f;
 
+    private void OnTriggerStay2D(Collider2D col) {
+        if(col.gameObject.tag == "Player") {
+            Debug.Log("Touched Player!");
+            GettingDamage(col);
+        } 
+    }
+
+    private void GettingDamage(Collider2D col) {
+        col.gameObject.GetComponent<MainCharacter>().GetDamage();
+    }
+
     void Update()
     {
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
--- Assets/Scripts/Copper Claw/BulletWaySpawn.cs
+++ Assets/Scripts/Copper Claw/BulletWaySpawn.cs
@@ -14,11 +14,14 @@ public class BulletWaySpawn : SpawnerDots
 
     private void BulletSpawn()
     {
+        Quaternion rotation = Quaternion.Euler(0,0,-90);
         _timer += Time.deltaTime;
 
         if(_timer >= _delay)
         {
-            Instantiate(_bullet, _points[Random.Range(0, _points.Length)].transform.position, Quaternion.identity);
+            Instantiate(_bullet, _points[0].transform.position, rotation);
+            Instantiate(_bullet, _points[1].transform.position, rotation);
+            Instantiate(_bullet, _points[2].transform.position, rotation);
             _timer = 0f;
         }
     }
